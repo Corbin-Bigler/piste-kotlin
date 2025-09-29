57e52656ed65eec3570b48ca1ebd5d30f9618739
Corbin Bigler <77686907+Corbin-Bigler@users.noreply.github.com>
2025-09-29 15:44:48 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
     `java-library`
-    `maven-publish`
 }
 
 val reverseDomain = "com.thysmesi"
@@ -16,26 +15,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
     testImplementation(kotlin("test"))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("gpr") {
-            from(components["java"])
-            groupId = reverseDomain
-            artifactId = "piste"
-            version = "0.0.1"
-        }
-    }
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/YOUR_USERNAME/YOUR_REPO")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USER")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
 }
 
 tasks.test {
